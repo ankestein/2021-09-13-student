4ccb3a72a8d9692e716ef3e0766186842f496bdb
Anke <a.stein@posteo.de>
2021-09-16 14:51:13 +0200

    Add case of adding List of students to AppMain for trying out try-catch

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -5,21 +5,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AppMain {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, "Laura"));
         students.add(new Student(2, "Tim"));
-
         StudentDB studentsDB = new StudentDB(students);
 
+        // try-catch for adding a single student
+/*
         try {
             studentsDB.add(new Student(1, "Laura"));
-        } catch (RuntimeException e) {
+        } catch (Exception e) {
+            // System.out.println("Something went wrong");
             // System.out.println(e);
             // System.err.println(e);
             e.printStackTrace();
         }
 
+
+        // try-catch for adding a list of students
+*/
+        List<Student> newStudents = new ArrayList<>();
+        newStudents.add(new Student(3, "John"));
+        newStudents.add(new Student(2, "Tim"));
+        StudentDB newStudentsDB = new StudentDB(newStudents);
+
+
+        try {
+            studentsDB.add(newStudents);
+        } catch (Exception e) {
+            System.err.println(e);
+            e.printStackTrace();
+        }
         System.out.println(studentsDB);
 
 
